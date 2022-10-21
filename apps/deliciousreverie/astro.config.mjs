32efee06c3c6775ee237846ffb71680867c140b3
Ben Read <endymion1818@gmail.com>
2022-10-21 19:30:30 +0100

    feature: init tailwind config

--- apps/deliciousreverie/astro.config.mjs
+++ apps/deliciousreverie/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   outDir: '../../dist/apps/deliciousreverie',
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    tailwind()
+  ]
 });
---
+++ apps/deliciousreverie/tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+    '../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {},
+}

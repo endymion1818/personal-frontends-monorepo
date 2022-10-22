dcb7e84b49748107f5dbff07e5c045b182b65cea
Ben Read <endymion1818@gmail.com>
2022-10-22 19:11:35 +0100

    feature: home page, footer tweaks

---
+++ apps/deliciousreverie/sitedata.js
@@ -0,0 +1,44 @@
+export let title = 'deliciousreverie';
+export let subtitle = "blog of developer & bookworm benjamin read"
+
+export let menuItems = [{
+  title: 'about',
+  link: '/about',
+}]
+
+export let footerMenuItems = [{
+  title: 'Links',
+  items: [{
+    title: 'Talks',
+    link: '/tag/talks'
+  }, {
+    title: 'Videos',
+    link: '/tag/videos'
+  },{
+    title: 'Articles',
+    link: '/tags/articles'
+  }]
+}, {
+  title: 'Menu',
+  items: [{
+    title: 'About',
+    link: '/about'
+  }]
+}]
+
+export let acknowledgementText = "Delicious Reverie is the blog of developer & bookworm benjamin read. Set in Lovechild and your system font. Built with Nx, Astro, Tailwind, DaisyUI and Svelte. Hosted by Netlify. © Some rights are reserved. This site doesn't use any cookies or other session storage and has no tracking scripts."
+
+export let logo = null
+
+export let socialLinks = [
+  {
+    title: "twitter",
+    link: "https://www.twitter.com/muzzlehatch_"
+  }, {
+    title: "reddit",
+    link: "https://www.reddit.com/u/endymion1818-1819"
+  }, {
+    title: "youtube",
+    link: "https://www.youtube.com/channel/UCzbL3ZYvJWzJbrbHCgZLDJQ"
+  }
+]

cf5b1460bce004460a3356cfd5c9cc96e3f67aa1
Ben Read <endymion1818@gmail.com>
2022-11-25 16:18:29 +0000

    fix(deliciousreverie): fix typography and accessibility issues

--- apps/deliciousreverie/sitedata.js
+++ apps/deliciousreverie/sitedata.js
@@ -1,5 +1,3 @@
-import Logo from "./src/components/logo.svg"
-
 export let title = 'Delicious Reverie';
 export let subtitle = "Benjamin Read's code garden."
 
@@ -38,8 +36,6 @@ export let footerMenuItems = [{
 }]
 
 export let acknowledgementText = "Delicious Reverie is the blog of Benjamin Read. Set in Lovechild and your system font. Built with Nx, Astro, Tailwind, DaisyUI and Svelte. © Some rights are reserved. This site doesn't use any cookies or session storage and has no tracking scripts, except when there are YouTube embeds."
-
-export let logo = Logo
 
 export let socialLinks = {
   linkedin: {

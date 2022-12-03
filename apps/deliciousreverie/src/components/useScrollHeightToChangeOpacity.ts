8ee05bf9fd1bc836fa0a1759d8c23fadd4288b33
Ben Read <endymion1818@gmail.com>
2022-12-03 14:49:47 +0000

    fix(components): same api for animations

--- apps/deliciousreverie/src/components/useScrollHeightToChangeOpacity.ts
+++ apps/deliciousreverie/src/components/useScrollHeightToChangeOpacity.ts
@@ -1,9 +1,9 @@
 import getScrollPercent from "./getScrollPercent"
 
-export function useScrollHeightToChangeOpacity(domElement, inHeight, outHeight){
+export default function useScrollHeightToChangeOpacity(domElement, inHeight, outHeight){
   if (getScrollPercent() > inHeight &&  getScrollPercent() < outHeight){
-  domElement.style.opacity = 0.8
+  domElement.classList.add('visible')
 } else {
-  domElement.style.opacity = 0
+  domElement.classList.remove('visible')
 }
 }

711e811c15efbd57ec0ad06d17b46e1618783a43
endymion1818 <endymion1818@gmail.com>
2023-02-09 23:52:27 +0000

    fix: upgrade postcss, tailwind and astro

--- apps/deliciousreverie/postcss.config.cjs
+++ apps/deliciousreverie/postcss.config.cjs
@@ -1,3 +1,10 @@
+const path = require('path');
+
 module.exports = {
-  plugins: [require('tailwindcss')],
+  plugins: {
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.cjs'),
+    },
+    autoprefixer: {},
+  },
 };
